b415b4dde03803c1aa72698492c22f3fb74b096f
Lucky  Odi <luckyodi310@gmail.com>
2025-09-22 11:39:59 +0100

    Modify main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from datetime import datetime, timezone, date, time
 from decimal import Decimal
 from typing import Optional, List
-
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
